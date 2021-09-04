16cf7737b2ac66c230c689c30ed936f82d61389b
Jasdeep Singh Dhillon <59929844+Jasdeep-Singh-Dhillon@users.noreply.github.com>
2021-09-04 07:01:32 +0530

    Implemented Intent factory test

--- app/src/androidTest/java/com/jasdeep/restapiapplication/IntentFactoryTest.java
+++ app/src/androidTest/java/com/jasdeep/restapiapplication/IntentFactoryTest.java
@@ -1,6 +1,7 @@
 package com.jasdeep.restapiapplication;
 
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -16,11 +17,18 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class IntentFactoryTest {
     @Test
-    public void useAppContext() {
+    public void getIntentTest() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.jasdeep.restapiapplication", appContext.getPackageName());
+
+        Intent intent = MainActivity.getIntent(appContext.getApplicationContext(),1, "Jasdeep");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        appContext.startActivity(intent);
+        assertEquals(1, intent.getIntExtra(Util.USER_KEY, -1));
+        assertEquals("Jasdeep", intent.getStringExtra(Util.USER_NAME));
     }
+
 }
